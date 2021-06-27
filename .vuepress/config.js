7b940fd7581b02744594d70e63d09f6496a3dcd0
hee-develop <hee950320@yahoo.co.jp>
2021-06-27 14:33:47 +0900

    add vuepress 1.x

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'TIL',
+  description: 'Today I learned by hee-develop',
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+    ],
+  },
+};
